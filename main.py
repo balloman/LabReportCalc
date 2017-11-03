072977aef347a4324c2102b85b4ad23329755a8b
Bernard Allotey <benard.allotey4@yahoo.com>
2017-11-02 23:51:47 -0500

    Fixed Text

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print "Enter temperature change of solution"
 temp = float(raw_input(">>> "))
 print "Enter price per 500g of solid"
 price = float(raw_input(">>> "))
-print "Enter mass of solid, leave blank for 5.00 g"
+print "Enter mass of solid"
 mass = float(raw_input(">>> "))
 
 tpredic = temp * 1.25 * (10/(mass+50))
